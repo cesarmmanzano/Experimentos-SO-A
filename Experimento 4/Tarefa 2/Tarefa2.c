1211058f720448b2d2c8b31039e8985fff2efe4e
Cesar Marrote Manzano <43895268+cesarmmanzano@users.noreply.github.com>
2020-04-18 22:50:02 -0300

    Add files via upload

--- Experimento 4/Tarefa 2/Tarefa2.c
+++ Experimento 4/Tarefa 2/Tarefa2.c
@@ -1,4 +1,8 @@
-/*
+/* ============================================================================================================== */
+
+/* gcc Tarefa2.c -o tar2 -pthread */
+
+/*	
  *	Programe a solução do jantar das filósofas, fazendo com que cada filósofa seja
  * 	uma thread. Para estabelecer exclusão mútua use mutex. Para isso, utilize as
  *	chamadas pthread_mutex_init(), pthread_mutex_lock(), pthread_mutex_unlock() e
@@ -7,7 +11,7 @@
  *	filósofa que chamou testa e como se encontram os estados das cinco filósofas.
  *	Cada filósofa deve terminar depois de ter comido 365 vezes. Use pthread_join()
  *	para aguardar o término das filósofas.
- */
+ */ 	
 
 
 /* ============================================================================================================== */
@@ -18,38 +22,61 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <locale.h>
 
 /* Constantes necessárias */
-/* Define numero de filosofos e quantas vezes cada um comerá */
 #define PHILOSOPHERS 5
 #define NO_OF_ITERATIONS 365
+#define THINKING_TIME 25
 
 /* Estados das filosofas */
 #define THINKING 0
 #define HUNGRY 1
 #define EATING 2
 
-/* Vetor para contar quantas vezes os filosofos ja comeram*/
-int eat[PHILOSOPHERS];
+/* Struct que armazena informações sobre as filosofas */
+typedef struct {
+  	int eat[PHILOSOPHERS];		/* Vetor para contar quantas vezes as filosofas ja comeram*/
+	int state[PHILOSOPHERS];	/* Vetor para armazenar o estado de cada filosofa */	
+}philo;
+philo info_philosophers;
 
 /* Threads e mutex */
-pthread_t philosophers[PHILOSOPHERS];
-pthread_mutex_t cutlery[PHILOSOPHERS];
-pthread_mutex_t mutex;
+pthread_t philosophers[PHILOSOPHERS];	/* Cada filosofa é uma thread */
+pthread_mutex_t cutlery[PHILOSOPHERS];	/* Trava os talheres*/
+pthread_mutex_t mutex;			/* Trava thread */
 
 
 /* ============================================================================================================== */
 
 
 /* Funções */
-void *philosophers_dinner(void * philosopherid);
+void *PhilosophersDinner(void * philosopherid);
+void PrintState();
+void CheckEat();
+
 
 /* ============================================================================================================== */
 
 
 int main(){
+  
+  setlocale(LC_ALL, "Portuguese");
+
   int i;
   int num_philosopher[PHILOSOPHERS];
+  
+  printf("\n");
+
+	/* Inicializa estado de cada filosofa (todas iniciam pensando)*/
+	for(i = 0; i < PHILOSOPHERS; i++){
+		info_philosophers.state[i] = THINKING;
+	}
+	
+	/* Inicializa quantidade de vezes que cada filosofa comeu */
+	for(i = 0; i < PHILOSOPHERS; i++){
+		info_philosophers.eat[i] = 0;
+	}
 
   	/* Inicializar mutex */
 	for(i = 0; i < PHILOSOPHERS; i++){
@@ -60,8 +87,8 @@ int main(){
   	/* Criar as 5 threads */
 	for (i = 0; i < PHILOSOPHERS; i++) {
 		num_philosopher[i] = i;
-		if(pthread_create(&philosophers[i], NULL, philosophers_dinner, (void *)&num_philosopher[i])){
-			printf("ERRO: impossivel criar a thread\n");
+		if(pthread_create(&philosophers[i], NULL, PhilosophersDinner, (void *)&num_philosopher[i])){
+			printf("ERRO: Impossível criar a thread\n");
       			exit(-1);
 		}
 	}
@@ -78,6 +105,7 @@ int main(){
     	pthread_mutex_destroy(&mutex);
 	
   /* Termina o programa */
+  printf("\n");
   exit(0);
 }
 
@@ -85,12 +113,55 @@ int main(){
 /* ============================================================================================================== */
 
 
-void *philosophers_dinner(void *philosopherid){
+void *PhilosophersDinner(void *philosopherid){
   int *p_id = philosopherid;
-	
-	/* Enquanto a filosofa nao comer 365 vezes */
-	while(eat[*p_id] < NO_OF_ITERATIONS){
+
+	/* Espera cada filosofa comer 365 vezes */
+	while(info_philosophers.eat[*p_id] < NO_OF_ITERATIONS){
+		/* Cada filosofa pensa e pega os talheres */
+		
+		/* Pensa */
+		info_philosophers.state[*p_id] = THINKING;
+		usleep(THINKING_TIME);
+		
+		/* Pega os talheres */
+		//TakeCutlery();
+		//PutCutlery();
+
 		break;
+	}
+	
+	/* Quando a filósofa acabar de comer, imprime */
+	printf("Filósofa %d acabou de comer\n", *p_id + 1);		
+
+	/* Encerra thread */
+	pthread_exit(NULL);
+}
+
+
+/* ============================================================================================================== */
+
+
+/* Função que verifica se a filosofa pode comer */
+void CheckEat(){
+	PrintState();
+}
+
+
+/* ============================================================================================================== */
+
+
+/* ============================================================================================================== */
+
+
+/* Função que printa o estado de cada filosofa */
+void PrintState(){
+	printf("\nEstado de cada filosofa");
+	
+	for(int i = 0; i < PHILOSOPHERS; i++){
+		if(info_philosophers.state[i] == THINKING) printf("Filósofa %d está pensando", i + 1);
+		if(info_philosophers.state[i] == HUNGRY) printf("Filósofa %d está com fome", i + 1);
+		if(info_philosophers.state[i] == EATING) printf("Filósofa %d está comendo", i + 1);
 	}
 }
 
